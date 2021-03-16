9fffa39b822462db433c9c6754948dcbb5fff1be
lukasberk <lukasvdberk@gmail.com>
2021-03-16 14:50:55 +0100

    [Added] Initial setup

---
+++ ServerSightPostScript/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerSightPostScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
